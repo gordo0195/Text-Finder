322c8d8012a9bee9402ed9cf591b4c5073237cad
gordo-0195 <gordo-0195@gordo0195-To-be-filled-by-O-E-M>
2019-10-15 23:47:35 -0600

    tird commit: add words to node of binary tree

--- src/arbol/LamadaPDF.java
+++ src/arbol/LamadaPDF.java
@@ -10,26 +10,48 @@ package arbol;
  * @author marco
  */
 import java.io.IOException;
+import java.util.Arrays;
 
 /**
  *
  * @author Azeem
  */
-public class LamadaPDF {
+public class LamadaPDF extends Nodo {
 
-    /**
-     * @param args the command line arguments
-     */
-    
     public static void main(String[] args) {
+        
+        
         LectorPDF pdfManager = new LectorPDF();
-        pdfManager.setFilePath("/home/marco/Descargas/Tarea #2 - Tutoria.pdf");
-        try {
+        pdfManager.setFilePath("/home/gordo-0195/Dropbox/Comenzar.pdf");
+        Arbol arbolIn = new Arbol();
+        try{
             String text = pdfManager.toText();
-            System.out.println(text);
+            String[] words = text.split("\\s+");
+            for (int i = 0; i < words.length; i++) {
+                 // You may want to check for a non-word character before blindly
+                // performing a replacement
+                // It may also be necessary to adjust the character class
+                 words[i] = words[i].replaceAll("[^\\w]", "");
+                 
+            }
+            
+            String temp2 = Arrays.toString(words);
+            //System.out.println(temp2);
+            
+            int tope = words.length;
+            for(int i = 0; i < tope; i++){
+                
+                //Nodo temporal = new Nodo();
+                arbolIn.insertar(words[i]);
+            
+            }
+            
         } catch (IOException ex) {
-           System.out.println(ex.getMessage());
+           //System.out.println(ex.getMessage());
         }
+        
+        System.out.print(arbolIn);
+    }
     }
 
-}
+   
